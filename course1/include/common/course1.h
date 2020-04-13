80937e79280782013dfaea07441c033f5f63bb03
AlphaArslan <arslan.alpha21@gmail.com>
2020-04-13 20:23:28 +0200

    cource1.h and memory.h

---
+++ course1/include/common/course1.h
@@ -0,0 +1,39 @@
+/**
+ * @file course1.h
+ * @brief Testing all functionalities
+ *
+ * This header file provides testing functions to verify the
+ * working of other functions like, my_memmove, my_memcopy and others
+ *
+ * @author Mohamed Said
+ * @date April 13 2020
+ *
+ */
+#ifndef __COURSE1_H__
+#define __COURSE1_H__
+#include "platform.h"
+
+/**
+ * @brief Calls different test functions
+ *
+ * This function is called by main(), and on its turn calls other
+ * test functions one by one
+ *
+ *
+ * @return int8_t   0  if successfull
+ *                  -1 if failed
+ */
+int8_t course1();
+
+/**
+ * @brief Tests non-overlapped memmove
+ *
+ * This function gives non-overlapping source and destination of
+ * a data block to memmove() function [memory.h]
+ *
+ * @return int8_t   0  if successfull
+ *                  -1 if failed
+ */
+int8_t test_memmove1();
+
+#endif /* __COURSE1_H__ */
--- course1/include/common/memory.h
+++ course1/include/common/memory.h
@@ -2,10 +2,10 @@
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
@@ -13,17 +13,20 @@
  * @brief Abstraction of memory read and write operations
  *
  * This header file provides an abstraction of reading and
- * writing to memory via function calls. 
+ * writing to memory via function calls.
  *
  * @author Alex Fosdick
  * @date April 1 2017
  *
+ * Modified by Mohamed Said, April,13,2020
+ *
  */
 #ifndef __MEMORY_H__
 #define __MEMORY_H__
+#include "platform.h"
 
 /**
- * @brief Sets a value of a data array 
+ * @brief Sets a value of a data array
  *
  * Given a pointer to a char data set, this will set a provided
  * index into that data set to the value provided.
@@ -37,7 +40,7 @@
 void set_value(char * ptr, unsigned int index, char value);
 
 /**
- * @brief Clear a value of a data array 
+ * @brief Clear a value of a data array
  *
  * Given a pointer to a char data set, this will clear a provided
  * index into that data set to the value zero.
@@ -50,7 +53,7 @@ void set_value(char * ptr, unsigned int index, char value);
 void clear_value(char * ptr, unsigned int index);
 
 /**
- * @brief Returns a value of a data array 
+ * @brief Returns a value of a data array
  *
  * Given a pointer to a char data set, this will read the provided
  * index into that data set and return the value.
@@ -81,7 +84,7 @@ void set_all(char * ptr, char value, unsigned int size);
  * @brief Clears elements in a data array
  *
  * Given a pointer to a char data set, this will set a clear a number
- * of elements given the size provided. Clear means to set to zero. 
+ * of elements given the size provided. Clear means to set to zero.
  *
  * @param ptr Pointer to data array
  * @param size Number of elements to set to zero
@@ -89,5 +92,122 @@ void set_all(char * ptr, char value, unsigned int size);
  * @return void.
  */
 void clear_all(char * ptr, unsigned int size);
+
+/**
+ * @brief Moves a block of Data
+ *
+ * Given a pointer to a uint8_t data block source address, the
+ * size of the block, and the destination address, this will move
+ * the data block to the new destination memory location
+ *
+ * @param src       Pointer to data black
+ * @param dst       Pointer to new data black locaiton
+ * @param length    number of elements in the data block
+ * @return uint8_t* Pointer to new data black locaiton
+ *                  or Null pointer if failed
+ */
+uint8_t * my_memmove(uint8_t * src, uint8_t * dst, size_t length);
+
+/**
+ * @brief Copies a block of Data
+ *
+ * Given a pointer to a uint8_t data block source address, the
+ * size of the block, and the destination address, this will copy
+ * the data block to the new destination memory location
+ *
+ * @param src       Pointer to data black
+ * @param dst       Pointer to new data black locaiton
+ * @param length    number of elements in the data block
+ * @return uint8_t* Pointer to new data black locaiton
+ *                  or Null pointer if failed
+ */
+uint8_t * my_memcopy(uint8_t * src, uint8_t * dst, size_t length);
+
+/**
+ * @brief Sets a block of Data to a value
+ *
+ * Given a pointer to a uint8_t data block source address, the
+ * size of the block, this will set all data elements to the
+ * desired uint8_t value
+ *
+ * @param src       Pointer to data black
+ * @param length    number of elements in the data block
+ * @param value     uint8_t value to be written in all elements
+ *
+ * @return uint8_t* Pointer to data black
+ *                  or Null pointer if failed
+ */
+uint8_t * my_memset(uint8_t * src, size_t length, uint8_t value);
+
+/**
+ * @brief Sets a block of Data to a value
+ *
+ * Given a pointer to a uint8_t data block source address, the
+ * size of the block, this will set all data elements to the
+ * desired uint8_t value
+ *
+ * @param src       Pointer to data black
+ * @param length    number of elements in the data block
+ * @param value     uint8_t value to be written in all elements
+ *
+ * @return uint8_t* Pointer to data black
+ *                  or Null pointer if failed
+ */
+uint8_t * my_memset(uint8_t * src, size_t length, uint8_t value);
+
+/**
+ * @brief Sets a block of Data to a Zero
+ *
+ * Given a pointer to a uint8_t data block source address, the
+ * size of the block, this will set all data elements to Zero
+ *
+ * @param src       Pointer to data black
+ * @param length    number of elements in the data block
+ *
+ * @return uint8_t* Pointer to data black
+ *                  or Null pointer if failed
+ */
+uint8_t * my_memzero(uint8_t * src, size_t length);
+
+/**
+ * @brief Reverses the order of all of the elements
+ *
+ * Given a pointer to a uint8_t data block source address, the
+ * size of the block, this will reverse the order of all of
+ * the elements
+ *
+ * @param src       Pointer to data black
+ * @param length    number of elements in the data block
+ *
+ * @return uint8_t* Pointer to data black
+ *                  or Null pointer if failed
+ */
+uint8_t * my_reverse(uint8_t * src, size_t length);
+
+/**
+ * @brief Allocates a number of words in dynamic memory
+ *
+ * Given the number of int32_t elements, this will reserve
+ * space in dynamic memory and the return the address to
+ * the first element
+ *
+ * @param length    number of elements
+ *
+ * @return int32_t* Pointer to reserved memory black
+ *                  or Null pointer if failed
+ */
+int32_t * reserve_words(size_t length);
+
+/**
+ * @brief Frees a dynamic memory allocation
+ *
+ * Given int32_t* pointer to memory location to be freed
+ * this will free the memory location
+ *
+ * @param src       Pointer to memory location
+ *
+ * @return void.
+ */
+void free_words(int32_t * src);
 
 #endif /* __MEMORY_H__ */
