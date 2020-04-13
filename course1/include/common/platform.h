cd5c6a5b8bc501b4da80f24c86863c3f66065ad6
AlphaArslan <arslan.alpha21@gmail.com>
2020-04-13 20:17:58 +0200

    Included [platform.h] stdint.h

--- course1/include/common/platform.h
+++ course1/include/common/platform.h
@@ -2,10 +2,10 @@
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 #ifndef __PLATFORM_H__
@@ -21,6 +21,7 @@
  Platform - HOST
 ******************************************************************************/
 #elif defined (HOST)
+#include <stdint.h>
 #include <stdio.h>
 #define PRINTF(...) printf(__VA_ARGS__)
 /******************************************************************************
@@ -31,4 +32,3 @@
 #endif
 
 #endif /* __PLATFORM_H__ */
-
