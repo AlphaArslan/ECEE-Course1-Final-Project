6a059129a1abdfa3d739c73053923dbb3d193173
AlphaArslan <arslan.alpha21@gmail.com>
2020-04-13 17:46:08 +0200

    updated sources.mk

--- course1/sources.mk
+++ course1/sources.mk
@@ -2,20 +2,22 @@
 # Copyright (C) 2017 by Alex Fosdick - University of Colorado
 #
 # Redistribution, modification or use of this software in source or binary
-# forms is permitted as long as the files maintain this copyright. Users are 
+# forms is permitted as long as the files maintain this copyright. Users are
 # permitted to modify this and use it to learn about the field of embedded
 # software. Alex Fosdick and the University of Colorado are not liable for any
-# misuse of this material. 
+# misuse of this material.
 #
 #*****************************************************************************
 
-SOURCES = 	main.c				\
-		memory.c			\
-		interrupts_msp432p401r_gcc.c	\
-		startup_msp432p401r_gcc.c	\
-		system_msp432p401r.c
+SOURCES =		src/main.c												\
+						src/memory.c											\
+						src/interrupts_msp432p401r_gcc.c	\
+						src/startup_msp432p401r_gcc.c			\
+						src/system_msp432p401r.c					\
+						src/data.c												\
+						src/course1.c											\
+						src/stats.c
 
-INCLUDES = 	-I../include/common			\
-		-I../include/msp432 			\
-		-I../include/CMSIS
-
+INCLUDES = 	-Iinclude/common								\
+						-Iinclude/msp432 								\
+						-Iinclude/CMSIS
