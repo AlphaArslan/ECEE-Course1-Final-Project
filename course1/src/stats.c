56dc5df547aab4ed3d08de61a816ef7f9e94c6a5
AlphaArslan <arslan.alpha21@gmail.com>
2020-04-13 20:17:08 +0200

    Removed [stats.c] main()

--- course1/src/stats.c
+++ course1/src/stats.c
@@ -21,30 +21,12 @@
 
 
 
-#include <stdio.h>
+#include "platform.h"
 #include "stats.h"
 
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
-
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
-
-  //sorting test array from largest to smallest
-  sort_array(test, SIZE);
-
-  /* Statistics and Printing Functions Go Here */
-  // printing sorted array
-  print_array(test, SIZE);
-  // printing statistical statistical values
-  print_statistics(test, SIZE);
-
-}
 
 void sort_array( unsigned char * pArray, char arraySize){
   unsigned char temp;
