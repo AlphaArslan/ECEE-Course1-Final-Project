f1e45693dcc986c49c3a404d6f847b97d68c46c6
AlphaArslan <arslan.alpha21@gmail.com>
2020-04-14 13:05:44 +0200

    Implemented my_itoa()

---
+++ course1/include/common/data.h
@@ -0,0 +1,46 @@
+/**
+ * @file data.h
+ * @brief Basic data manipulation
+ *
+ * This header file provides basic data manipulation functions
+ *
+ * @author Mohamed Said
+ * @date April 14 2020
+ *
+ */
+#ifndef __DATA_H__
+#define __DATA_H__
+#include "platform.h"
+
+/**
+ * @brief converts integer to ASCII string
+ *
+ * This function converts an integer (ex, 1234) to ASCII string
+ * representaion (ex, "1234"). It supports signed numbers and up
+ * to base of 16 (Hexadecimal).
+ *
+ * @param   data  int32_t number to be converted
+ * @param   ptr   uint8_t* pointer to memory space where ASCII
+ *                string gets stored
+ * @param   base  uint32_t number base. Ex, 2 (Binary), 10 (decimal)
+ *
+ * @return uint8_t   number of characters (including NULL character)
+ *                    -1 if failed
+ */
+uint8_t my_itoa(int32_t data, uint8_t * ptr, uint32_t base);
+
+/**
+ * @brief converts ASCII string to integer
+ *
+ * This function converts an ASCII string (ex, "1234") to
+ * integer (ex, 1234). It supports signed numbers and up
+ * to base of 16 (Hexadecimal).
+ *
+ * @param   ptr   uint8_t* pointer ASCII string to be converted
+ * @param  digits uint8_t number of digits in the ASCII string
+ * @param   base  uint32_t number base. Ex, 2 (Binary), 10 (decimal)
+ *
+ * @return int32_t   integer value after conversion
+ */
+int32_t my_atoi(uint8_t * ptr, uint8_t digits, uint32_t base);
+#endif /* __DATA_H__ */
---
+++ course1/src/data.c
@@ -0,0 +1,45 @@
+/**
+ * @file data.c
+ * @brief Basic data manipulation operations
+ *
+ * This implementation file provides functions to convert data
+ * from integer representaion to ASCII string and vice versa.
+ *
+ * @author Mohamed Said
+ * @date April 14 2020
+ *
+ */
+#include "data.h"
+#define   POW(x, y)   pow((float)y,(float)y)
+
+/***********************************************************
+ Function Definitions
+***********************************************************/
+uint8_t my_itoa(int32_t data, uint8_t * ptr, uint32_t base){
+  // check on the sign
+  if(data > 0){                                   // positive
+    uint8_t index = 0;                                  // indicating the memory location
+    // find out how many digits
+    int8_t digit_count = 0;
+    int32_t temp = data;
+    do{digit_count++; temp /= base;}while(temp != 0);
+    // convert to desired base and store in ASCII representaion
+    temp = data;
+    for(digit_count -= 1; digit_count >= 0; digit_count--, index++){
+      // '0' is 48 in ascii table
+      *(ptr + index) = 48 + (uint8_t)temp / (uint8_t)POW(base, digit_count);
+      temp -= POW(base, digit_count);
+    }
+    *(ptr + index) = 0;                                 // Null character at the end
+    return index;                                       // return string length
+  }else{                                          // negative
+    data += 2*data;                                     // convert to positive
+    // put a minus sign "-" at the start
+    *ptr = 45;                                          // '-' is 45 in ascii table
+    return my_itoa(data, ptr+1, base)+1;
+  }
+}
+
+int32_t my_atoi(uint8_t * ptr, uint8_t digits, uint32_t base){
+  return 0;
+}
